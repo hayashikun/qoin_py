680edfce48af43da876d86addbe5e2e784ddd31c
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-08-27 22:17:18 +0900

    Enable to detect left or right

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class QoinPy:
         for res in response:
             print(res)
 
-    def run_face_mesh(self):
+    def _face_mesh(self, receive_handler, post_handler):
         """
         Run face_mesh
         :return:
@@ -31,8 +31,69 @@ class QoinPy:
         from proto.qoin.proto import face_mesh_pb2, face_mesh_pb2_grpc
         stub = face_mesh_pb2_grpc.FaceMeshStub(self._channel)
         response = stub.FaceMeshStream(face_mesh_pb2.FaceMeshRequest())
-        for res in response:
-            print(res)
+        try:
+            for res in response:
+                xyz = np.array([
+                    [[lm.x, lm.y, lm.z] for lm in lml.landmark]
+                    for lml in res.landmark_list])
+                receive_handler(xyz)
+        except Exception as e:
+            print(e)
+        post_handler()
+
+    def run_face_mesh(self, landmark_save_path=None, suppress_dump=False):
+        landmarks = list()
+
+        def receive_handler(xyz):
+            if not suppress_dump:
+                print(xyz)
+            if landmark_save_path is not None:
+                landmarks.append(xyz)
+
+        def post_handler():
+            if landmark_save_path is not None:
+                np.save(landmark_save_path, np.array(landmarks))
+
+        self._face_mesh(receive_handler, post_handler)
+
+    def right_or_left(self):
+        def diff(lm):
+            hist, x, y = np.histogram2d(lm[:, 0], lm[:, 1], bins=10)
+            return hist[: len(x) // 2, :].sum() - hist[len(x) // 2:, :].sum()
+
+        sampling_len = 20
+        state = {
+            "avg": 0,
+            "sampling": np.empty(shape=sampling_len),
+            "idx": 0,
+            "direction": "center"
+        }
+        threshold = 300
+
+        def receive_handler(xyz):
+            d = diff(xyz[0])
+            if state["idx"] < 20:
+                state["sampling"][state["idx"]] = d
+            elif state["idx"] == 20:
+                state["avg"] = state["sampling"].mean()
+                print("sampling done")
+            else:
+                d -= state["avg"]
+                if state["direction"] == "center" and d < -threshold:
+                    state["direction"] = "right"
+                    print("right")
+                elif state["direction"] == "center" and d > threshold:
+                    state["direction"] = "left"
+                    print("left")
+                elif state["direction"] != "center" and np.abs(d) < threshold:
+                    state["direction"] = "center"
+
+            state["idx"] += 1
+
+        def post_handler():
+            pass
+
+        self._face_mesh(receive_handler, post_handler)
 
     def _hand_tracking(self, receive_handler, post_handler):
         from proto.qoin.proto import hand_tracking_pb2, hand_tracking_pb2_grpc
