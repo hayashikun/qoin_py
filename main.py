ec8206fb0afec6ec3cbad6311c1c0eb1f35dce94
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-08-27 18:59:36 +0900

    Use fire and make better

--- main.py
+++ main.py
@@ -1,72 +1,90 @@
+import fire
 import grpc
 import numpy as np
 import pyautogui
 
 
-def run_hello():
-    from proto.qoin.proto import hello_pb2_grpc, hello_pb2
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = hello_pb2_grpc.GreeterStub(channel)
-    response = stub.SayHello(hello_pb2.HelloRequest(name='you'))
-    print("Greeter client received: ", response.message)
-    response = stub.SayHelloAgain(hello_pb2.HelloRequest(name='you'))
-    print("Greeter client received: ", response.message)
-    response = stub.HelloStream(hello_pb2.HelloRequest(name='hayashi'))
-    for res in response:
-        print(res)
+class QoinPy:
+    def __init__(self, host="localhost", port=50051):
+        self._channel = grpc.insecure_channel(f'{host}:{port}')
 
-
-def run_face_mesh():
-    from proto.qoin.proto import face_mesh_pb2, face_mesh_pb2_grpc
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = face_mesh_pb2_grpc.FaceMeshStub(channel)
-    response = stub.FaceMeshStream(face_mesh_pb2.FaceMeshRequest())
-    for res in response:
-        print(res)
-
-
-def _hand_tracking(receive_handler, post_handler):
-    from proto.qoin.proto import hand_tracking_pb2, hand_tracking_pb2_grpc
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = hand_tracking_pb2_grpc.HandTrackingStub(channel)
-    response = stub.HandTrackingStream(hand_tracking_pb2.HandTrackingRequest())
-    try:
+    def run_hello(self):
+        """
+        Run hello
+        :return:
+        """
+        from proto.qoin.proto import hello_pb2_grpc, hello_pb2
+        stub = hello_pb2_grpc.GreeterStub(self._channel)
+        response = stub.SayHello(hello_pb2.HelloRequest(name='you'))
+        print("Greeter client received: ", response.message)
+        response = stub.SayHelloAgain(hello_pb2.HelloRequest(name='you'))
+        print("Greeter client received: ", response.message)
+        response = stub.HelloStream(hello_pb2.HelloRequest(name='hayashi'))
         for res in response:
-            pos = np.array([[lm.x, lm.y, lm.z] for lm in res.landmark_list.landmark])
-            receive_handler(pos)
-    except Exception as e:
-        print(e)
-    post_handler()
+            print(res)
 
+    def run_face_mesh(self):
+        """
+        Run face_mesh
+        :return:
+        """
+        from proto.qoin.proto import face_mesh_pb2, face_mesh_pb2_grpc
+        stub = face_mesh_pb2_grpc.FaceMeshStub(self._channel)
+        response = stub.FaceMeshStream(face_mesh_pb2.FaceMeshRequest())
+        for res in response:
+            print(res)
 
-def run_hand_tracking():
-    positions = list()
+    def _hand_tracking(self, receive_handler, post_handler):
+        from proto.qoin.proto import hand_tracking_pb2, hand_tracking_pb2_grpc
+        stub = hand_tracking_pb2_grpc.HandTrackingStub(self._channel)
+        response = stub.HandTrackingStream(hand_tracking_pb2.HandTrackingRequest())
+        try:
+            for res in response:
+                xyz = np.array([[lm.x, lm.y, lm.z] for lm in res.landmark_list.landmark])
+                receive_handler(xyz)
+        except Exception as e:
+            print(e)
+        post_handler()
 
-    def receive_handler(pos):
-        print(pos)
-        positions.append(pos)
+    def run_hand_tracking(self, landmark_save_path=None, suppress_dump=False):
+        """
+        Run hand_tracking
+        :param landmark_save_path: If not None, landmark xyz data is saved at the path.
+        :param suppress_dump: If True, landmark xyz is not dumped.
+        :return:
+        """
+        landmarks = list()
 
-    def post_handler():
-        pass
-        # np.save("positions", np.array(positions))
+        def receive_handler(xyz):
+            if not suppress_dump:
+                print(xyz)
+            if landmark_save_path is not None:
+                landmarks.append(xyz)
 
-    _hand_tracking(receive_handler, post_handler)
+        def post_handler():
+            if landmark_save_path is not None:
+                np.save(landmark_save_path, np.array(landmarks))
 
+        self._hand_tracking(receive_handler, post_handler)
 
-def move_cursor():
-    screen_x, screen_y = pyautogui.size()
-    pyautogui.FAILSAFE = False
+    def move_cursor_with_hand(self):
+        """
+        Move mouse cursor with hand using hand_tracking
+        :return:
+        """
+        screen_x, screen_y = pyautogui.size()
+        pyautogui.FAILSAFE = False
 
-    def receive_handler(pos):
-        xy = pos.mean(axis=0)
-        if (xy > 0.0).all():
-            pyautogui.moveTo(screen_x * xy[0], screen_y * xy[1], duration=0)
+        def receive_handler(pos):
+            xy = pos.mean(axis=0)
+            if (xy > 0.0).all():
+                pyautogui.moveTo(screen_x * xy[0], screen_y * xy[1], duration=0)
 
-    def post_handler():
-        pass
+        def post_handler():
+            pass
 
-    _hand_tracking(receive_handler, post_handler)
+        self._hand_tracking(receive_handler, post_handler)
 
 
 if __name__ == '__main__':
-    run_hand_tracking()
+    fire.Fire(QoinPy)
