2476374f39e002e4a488c699cb84499ff476f667
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-08-30 22:29:09 +0900

    Add babiniku

--- main.py
+++ main.py
@@ -95,6 +95,96 @@ class QoinPy:
 
         self._face_mesh(receive_handler, post_handler)
 
+    def babiniku(self):
+        """
+        Be bisyoujo
+        
+        :return:
+        """
+        import cv2
+        from face_mesh_parts import landmark_parts_index
+        raw_img = cv2.imread("res/face_base.png", -1)
+
+        img_size = (1000, 1000)
+        base_face_center = (500, 590)
+        base_face_size = (280, 260)
+
+        raw_img = cv2.resize(raw_img, img_size)
+        raw_img[raw_img[:, :, -1] == 0] = 220, 200, 255, 255
+
+        eye_size = (100, 60)
+        left_eye = cv2.imread("res/left_eye.png", -1)
+        right_eye = cv2.imread("res/right_eye.png", -1)
+        eyes_img = [cv2.resize(left_eye, eye_size), cv2.resize(right_eye, eye_size)]
+
+        def receive_handler(xyz):
+            x, y = xyz[0][:, :2].T
+            x -= x.mean()
+            y -= y.mean()
+            img = raw_img.copy()
+
+            lips_y = np.array([
+                y[landmark_parts_index["upper_lip"]].mean(), y[landmark_parts_index["lower_lip"]].mean()
+            ])
+
+            l_idx = landmark_parts_index["left_eye"]
+            r_idx = landmark_parts_index["right_eye"]
+            eyes = np.array([
+                [x[l_idx].mean(), y[l_idx].mean()],
+                [x[r_idx].mean(), y[r_idx].mean()],
+            ])
+            face_angle = -np.arctan((eyes[1, 1] - eyes[0, 1]) / (eyes[1, 0] - eyes[0, 0]))
+
+            f_idx = landmark_parts_index["face_oval"]
+            face_center = np.array([x[f_idx].mean(), y[f_idx].mean()])
+            mat = cv2.getRotationMatrix2D(tuple(face_center), np.rad2deg(- face_angle), 1)
+            reversed_face_pts = mat @ np.array([
+                x[f_idx],
+                y[f_idx],
+                np.ones_like(f_idx)
+            ])
+            x_scale = base_face_size[0] / (reversed_face_pts[0, :].max() - reversed_face_pts[0, :].min())
+            y_scale = base_face_size[1] / (reversed_face_pts[1, :].max() - reversed_face_pts[1, :].min())
+
+            for e, ei in zip(eyes, eyes_img):
+                ex, ey = mat @ np.array([e[0], e[1], 1])
+                ex = int(ex * x_scale + base_face_center[0])
+                ey = int(ey * y_scale + base_face_center[1])
+                sx = ex - eye_size[0] // 2, ex + eye_size[0] // 2
+                sy = ey - eye_size[1] // 2, ey + eye_size[1] // 2
+                tr = ei[:, :, -1] / 255
+                for i in range(3):
+                    img[sy[0]:sy[1], sx[0]:sx[1], i] = ei[:, :, i] * tr + img[sy[0]:sy[1], sx[0]:sx[1], i] * (1 - tr)
+
+            lips_center = (base_face_center[0], (base_face_center[1] + lips_y[1] * y_scale).astype(np.int))
+            lips_radius = np.abs((lips_y[1] - lips_y[0]) * y_scale).astype(np.int)
+            img = cv2.circle(img, lips_center, lips_radius, color=(180, 120, 255), thickness=-1)
+            img = cv2.circle(img, lips_center, lips_radius, color=(0, 0, 0), thickness=1)
+
+            l_idx = landmark_parts_index["left_eyebrow"]
+            r_idx = landmark_parts_index["right_eyebrow"]
+            reversed_eyebrow_pts = [
+                mat @ np.array([x[l_idx], y[l_idx], np.ones_like(l_idx)]),
+                mat @ np.array([x[r_idx], y[r_idx], np.ones_like(r_idx)])
+            ]
+
+            for eb in reversed_eyebrow_pts:
+                ebx = (eb[0] * x_scale + base_face_center[0])
+                eby = (eb[1] * y_scale + base_face_center[1])
+                eby = (eby.min() + (eby - eby.min()) * 0.25)
+                img = cv2.fillConvexPoly(img, np.array([ebx.astype(np.int), eby.astype(np.int)]).T, color=(50, 50, 50))
+
+            mat = cv2.getRotationMatrix2D((img_size[0] // 2, img_size[1]), np.rad2deg(face_angle) // 4, 1)
+            img = cv2.warpAffine(img, mat, img_size)
+
+            cv2.imshow("babiniku", img)
+            cv2.waitKey(1)
+
+        def post_handler():
+            pass
+
+        self._face_mesh(receive_handler, post_handler)
+
     def _hand_tracking(self, receive_handler, post_handler):
         from proto.qoin.proto import hand_tracking_pb2, hand_tracking_pb2_grpc
         stub = hand_tracking_pb2_grpc.HandTrackingStub(self._channel)
